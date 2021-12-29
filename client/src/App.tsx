6bb48decec26658b2cab345aec9c163e0942bdcc
Joakim <j-pf@live.se>
2021-12-29 18:25:04 +0100

    made to work with context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Layout from './components/layout/layout';
-import firebaseCollection from './firebase';
-import { getFirestore } from "firebase/firestore"
-import { collection, getDocs } from "firebase/firestore"; 
-interface User {
+//import firebaseCollection from './firebase';
+//import { getFirestore } from "firebase/firestore"
+//import { collection, doc, getDocs, setDoc } from "firebase/firestore"; 
+import FirebaseProvider from './context/firebaseProvider';
+
+
+/* interface User {
     firstName: string,
     lastName: string,
-    userName: string
-}
+    userName: string,
+	id: string
+} */
 
 function App() {
 
-	const [users, setUsers] = useState<User[]>()
-
-	
-	console.log(firebaseCollection.db)
-
-	async function getUsers() {
-
-		const userCol = collection(firebaseCollection.db, 'users');
-		const userSnapshot = await getDocs(userCol);
-		const userList = userSnapshot.docs.map(doc => doc.data() as User);
-		setUsers(userList)
-		
-  	}
-
-	useEffect(() => {
-		getUsers()
-	}, [])
-	console.log(users)
-	
   return (
-    <Layout />
+	<FirebaseProvider>
+		<Layout />
+	</FirebaseProvider>
   );
 }
 
