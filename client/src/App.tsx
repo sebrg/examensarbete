93f052384458a1a8a1888616f083edcb712dec2f
Joakim <j-pf@live.se>
2021-12-30 15:45:32 +0100

    removed bloat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import Layout from './components/layout/layout';
-//import firebaseCollection from './firebase';
-//import { getFirestore } from "firebase/firestore"
-//import { collection, doc, getDocs, setDoc } from "firebase/firestore"; 
 import FirebaseProvider from './context/firebaseProvider';
 
 
-/* interface User {
-    firstName: string,
-    lastName: string,
-    userName: string,
-	id: string
-} */
+
 
 function App() {
 
-  return (
-	<FirebaseProvider>
-		<Layout />
-	</FirebaseProvider>
-  );
+	return (
+		<FirebaseProvider>
+			<Layout />
+		</FirebaseProvider>
+	);
 }
 
 export default App;
