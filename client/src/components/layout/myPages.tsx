90e9cb27d3d9cc18b0b1278b4c587476b64cf685
Joakim <j-pf@live.se>
2022-01-03 10:55:29 +0100

    made new components for routes

---
+++ client/src/components/layout/myPages.tsx
@@ -0,0 +1,17 @@
+import React, { CSSProperties, useContext, useEffect, useState } from 'react';
+import { FirebaseContext, FirebaseOptions } from '../../context/firebaseContext';
+
+
+
+
+export default function MyPages() {
+
+   
+
+
+
+    return (
+        <p>My pages</p>
+    );
+}
+
---
+++ client/src/components/layout/start.tsx
@@ -0,0 +1,17 @@
+import React, { CSSProperties, useContext, useEffect, useState } from 'react';
+import { FirebaseContext, FirebaseOptions } from '../../context/firebaseContext';
+
+
+
+
+export default function Start() {
+
+   
+
+
+
+    return (
+     <p>Start</p>
+    );
+}
+
