be47f66f51a9b8ef4cee754966faaf4330f61922
Joakim <j-pf@live.se>
2022-01-10 10:24:28 +0100

    added logo

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,9 +1,10 @@
 import React, { CSSProperties, useContext } from 'react';
 import MainNav from './mainNav';
-
+import marungoFatGreen from "./../../assets/cartLogoWithOutText.png"
 type Props = {
 	setLoginToggle: any
 	isLoggedIn: boolean | undefined
+	scrollContentIntoView?: (always: boolean) => void
 }
 
 export default function Header(props: Props) {
@@ -11,7 +12,8 @@ export default function Header(props: Props) {
 
     return (
 		<header style={headerStyle}>
-			<MainNav isLoggedIn={props.isLoggedIn} setLoginToggle={props.setLoginToggle} />
+			<MainNav scrollContentIntoView={props.scrollContentIntoView} isLoggedIn={props.isLoggedIn} setLoginToggle={props.setLoginToggle} />
+			<img src={marungoFatGreen} style={logoStyleTest}></img>
 		</header>
     );
 }
@@ -21,15 +23,19 @@ const headerStyle: CSSProperties = {
 	width: "100%",
 	minHeight: "15%",
 	backgroundColor: "#FFFFFF",
-	/*   borderTopLeftRadius: "15px",
-	borderTopRightRadius: "15px", */
-	padding: "10px",
+	/* padding: "10px", */
 	background: "#eea47fff",
-	//borderBottom: "2px solid black",
-	boxShadow: "0px 5px 5px 0px black",
-	WebkitBoxShadow: "0px 5px 5px 0px black",
+	boxShadow: "0px 5px 5px -2px black",
+	WebkitBoxShadow: "0px 5px 5px -2px black",
 	zIndex: 10,
 	position: "sticky",
 	top: 0,
+	scrollSnapAlign: "start",
 }
 
+const logoStyleTest: CSSProperties = {
+	objectFit: "contain", 
+	borderRadius: "15px", 
+/* 	border: "2px solid black", 
+	boxShadow: "inset 0 0 10px black" */
+}
