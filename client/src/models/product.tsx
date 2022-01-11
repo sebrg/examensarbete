ef5c4ab7e58493a5395c04efd3e72f87af9fc34f
Joakim <j-pf@live.se>
2022-01-11 16:17:11 +0100

    updated product model to consider img array

--- client/src/models/product.tsx
+++ client/src/models/product.tsx
@@ -1,9 +1,11 @@
 export class Product { 
     name: string; 
     price: number;
+    img: any[];
 
-    constructor(name: string, price: number) {
+    constructor(name: string, price: number, img: any) {
         this.name = name
         this.price = price
+        this.img = img
     }
 }
