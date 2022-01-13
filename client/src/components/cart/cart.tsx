7438cad78f556035d1c5aa7a8dbcc0b93739bc0e
Joakim <j-pf@live.se>
2022-01-13 17:36:31 +0100

    cart working

--- client/src/components/cart/cart.tsx
+++ client/src/components/cart/cart.tsx
@@ -2,41 +2,82 @@ import { DocumentData, documentId } from 'firebase/firestore';
 import React, { CSSProperties, useContext, useEffect, useState } from 'react';
 import { FirebaseOptions, FirebaseContext } from '../../context/firebaseContext';
 import { ProductContext, ProductOptions } from '../../context/products/productContext';
+import { Product } from '../../models';
+import ProductCard from '../UI/productCard';
+import CartProductController from './cartProductController';
 
+/**
+ * The cart synchronize ID'S saved in the localstorage to fetch the correct product data from the DB and 
+ * then combine the quantity from localstorage with the product data from DB into state("productsInCart")
+ * */ 
 export default function Cart() {
 
-    const [products, setProducts] = useState<DocumentData[]>()
-    const fbFuncs: FirebaseOptions = useContext(FirebaseContext)
+    const [productsInCart, setProductsInCart] = useState<Product[]>()
     const productContext: ProductOptions = useContext(ProductContext)
 
-    //productContext.functions.getAllProducts()
-    
-    const getCart = async () => {
+    const syncCart = async () => {
         let localst: string | null = localStorage.getItem('cart')
         if(localst) {
             let parsedLocal = JSON.parse(localst)
-            let cart = await productContext.functions.getProducts("products", documentId(), 'in', parsedLocal)
-            console.log(cart, "products in cart")
+            if(parsedLocal.length) {
+                
+                let cart = await productContext.functions.getProducts("products", documentId(), 'in', parsedLocal.map((localItem: any) => { return localItem.id }))
+                cart.map((cartItem: any) => {
+                    let foundItem = parsedLocal.find((localItem: any) => localItem.id == cartItem.id)
+                    if(foundItem) {
+                        cartItem.quantity = foundItem.quantity
+                    }
+                }) 
+                setProductsInCart(cart)
+            } 
+            else {
+                setProductsInCart(undefined)
+            }
         }
     }
 
     useEffect(() => {
-        getCart()
+        syncCart()
     }, [])
 
     useEffect(() => {
-        console.log(productContext.allProducts)
-    }, [productContext.allProducts])
+        console.log(productsInCart)
+    }, [productsInCart])
 
 
-    const getProducts = async () => {
-            const products = await fbFuncs.getAllProducts()
-            setProducts(products) 
-    }
+    
 
     return (
-		<div>
-            dad
+		<div id="cartProductsWrapper" style={cartProductsWrapperStyle}>
+            { 
+                productsInCart?    
+                    productsInCart.map((product, i) => {
+                            return(
+                                <ProductCard key={i} 
+                                    bgColor='#EFE1CE'
+                                    product={product as Product} 
+                                    width='15%'
+                                    height='100%'
+                                    imgWidth='100%'
+                                    imgHeight='auto'
+                                    linkTo={`${product.name}-${product.id}`}
+                                    displayProductInfo={false}
+                                >     
+                                    
+                                    <CartProductController product={product} syncCart={syncCart}/>
+
+                                </ProductCard>
+                            )
+                        })
+                    :
+                    <p>*SPINNER* ELLER "VARUKORGEN ÄR TOM"</p>
+            } 
         </div>
     );
+}
+
+const cartProductsWrapperStyle: CSSProperties = {
+    width: "100%",
+    display: "flex",
+    
 }
