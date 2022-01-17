57bea79c6a6c51bf7dc28db321ef3b9e1a80091b
Joakim <j-pf@live.se>
2022-01-17 14:59:09 +0100

    added "info" to product model

--- client/src/models/product.tsx
+++ client/src/models/product.tsx
@@ -8,13 +8,15 @@ export class Product {
     id?: string
 
     quantity?: number
+    info?: string
 
-    constructor(name: string, price: number, imgs: any, company?: string, id?: string, quantity?: number) {
+    constructor(name: string, price: number, imgs: any, company?: string, id?: string, quantity?: number, info?: string) {
         this.name = name
         this.price = price
         this.images = imgs
         this.company = company
         this.id = id
         this.quantity = quantity
+        this.info = info
     }
 }
