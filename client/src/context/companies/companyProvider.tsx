c16f2e60b4fe2a0f056c66db05752bf03da4cc28
Joakim <j-pf@live.se>
2022-01-18 18:17:04 +0100

    added functions

--- client/src/context/companies/companyProvider.tsx
+++ client/src/context/companies/companyProvider.tsx
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-import { addDoc, collection, doc, DocumentData, FieldPath, getDoc, getDocs, query, where, WhereFilterOp } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, DocumentData, documentId, FieldPath, getDoc, getDocs, query, where, WhereFilterOp } from "firebase/firestore";
 import React, { Component } from "react"
 import firebaseCollection from "../../firebase";
 import { CompanyContext, CompanyOptions, } from "../companies/companyContext"
@@ -13,24 +13,26 @@ export default class CompanyProvider extends Component<Props, CompanyOptions>   
         addCompany: this.addCompany.bind(this),
         getCurrentUserCompany: this.getCurrentUserCompany.bind(this),
         getAllCompanies: this.getAllCompanies.bind(this),
-        getCompany: this.getCompany.bind(this)
+        getCompany: this.getCompany.bind(this),
+        aproveCompany: this.aproveCompany.bind(this),
+        removeCompany: this.removeCompany.bind(this)
     }
 
-
-
-
-    
-    async addCompany(company: Company) {
+    async addCompany(company: Company, to: "companies" | "pendingCompanies") {
         //TODO: Fix better failsafe
-        /* if(company.category == "" || company.name == "" || company.region == "" || company.school == "") {
-            console.log("Fill all inputs")
-        } */
+       
+        let companyData: Omit<Company, "id"> = { // FORTSÄTT HÄR
+            category: company.category,
+            name: company.name,
+            region: company.region,
+            school: company.school
+        }
 
         const auth = getAuth();
 
         if(auth.currentUser) {
-            await addDoc(collection(firebaseCollection.db, "companies"), {
-                ...company, creator: auth.currentUser.uid
+            await addDoc(collection(firebaseCollection.db, to), {
+                ...companyData, creator: auth.currentUser.uid
             });
         }
         else {
@@ -41,31 +43,32 @@ export default class CompanyProvider extends Component<Props, CompanyOptions>   
 
     async getCurrentUserCompany() {
         const auth = getAuth();
-        const q = query(collection(firebaseCollection.db, "companies"), where("creator", "==", auth.currentUser?.uid));
+        
+        const q = query(collection(firebaseCollection.db, "companies"), where("creator", "==", auth?.currentUser?.uid));
         const querySnapshot = await getDocs(q);
-        const result: DocumentData[] = []
+        const result: Company[] = []
 
         querySnapshot.forEach((doc) => {
              // doc.data() is never undefined for query doc snapshots  
              //console.log({id: doc.id, data: doc.data()});
-             result.push({id: doc.id, data: doc.data()})
+             result.push({id: doc.id, ...doc.data()} as Company)
         });
 
-        return result
+        return result as Company[]
     }
 
-    async getAllCompanies() {
-        const result: DocumentData[] = []
-        const get = await getDocs(collection(firebaseCollection.db, "companies"));
+    async getAllCompanies(company: "companies" | "pendingCompanies") {
+        const result: Company[] = [] 
+        const get = await getDocs(collection(firebaseCollection.db, company));
         get.forEach((doc) => {
-            result.push({id: doc.id, data: doc.data()})
+            result.push({id: doc.id, ...doc.data()} as Company) 
           });
           return result
     }
 
-    async getCompany(fieldPath: string | FieldPath, opStr: WhereFilterOp, value: string | string[]) {
+    async getCompany(from: "companies" | "pendingCompanies", fieldPath: string | FieldPath, opStr: WhereFilterOp, value: string | string[]) {
         //FIXME: remove param dbCollection and replace with "products"
-        const q = query(collection(firebaseCollection.db, "companies"), where(fieldPath, opStr, value));
+        const q = query(collection(firebaseCollection.db, from), where(fieldPath, opStr, value));
         const querySnapshot = await getDocs(q);
         const result: Company[] = []
         
@@ -78,6 +81,19 @@ export default class CompanyProvider extends Component<Props, CompanyOptions>   
        return result
     }
 
+    async aproveCompany(id: string) {
+        const currentPendingCompany: Company[] = await this.getCompany("pendingCompanies", documentId(), "==", id)
+        console.log(currentPendingCompany)
+        
+        await this.addCompany(new Company(currentPendingCompany[0].name, currentPendingCompany[0].school, currentPendingCompany[0].region, currentPendingCompany[0].category ), "companies")
+        await this.removeCompany(id)
+        console.log("company aproved")
+    }
+    
+    async removeCompany(id: string) {
+        await deleteDoc(doc(firebaseCollection.db, "pendingCompanies", id));
+        console.log("company deleted")
+    }
     
     render() {
         return(
