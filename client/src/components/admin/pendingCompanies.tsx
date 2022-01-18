f7d6792f7ba7d9043f29405e294443ccb2dc204d
Joakim <j-pf@live.se>
2022-01-18 18:11:05 +0100

    added list with pending companies

---
+++ client/src/components/admin/pendingCompanies.tsx
@@ -0,0 +1,55 @@
+import React, { CSSProperties, useContext, useEffect, useState } from 'react';
+import { CompanyContext, CompanyOptions } from '../../context/companies/companyContext';
+import { FirebaseContext, FirebaseOptions } from '../../context/firebaseContext';
+import { Company } from '../../models';
+import marungTestBackground from "./../../assets/cartLogoDarker.png"
+import FoldableCompanyCard from './foldableCompanyCard';
+
+
+
+
+export default function PendingCompanies() {
+
+
+    const companyContext: CompanyOptions = useContext(CompanyContext)
+    const [pendingCompanies, setPendingCompanies] = useState<Company[]>()
+
+
+    const getPendingCompanies = async () => {
+        const allPendingCompanies = await companyContext.getAllCompanies('pendingCompanies')
+        if(allPendingCompanies) {
+            setPendingCompanies(allPendingCompanies)
+        }
+    }
+
+    useEffect(() => {
+        getPendingCompanies()
+    }, [])
+    
+    useEffect(() => {
+        console.log(pendingCompanies)
+    }, [pendingCompanies])
+
+
+
+    return (
+        <div id="pendingCompanies" style={pendingCompaniesStyle}>
+            {pendingCompanies?.length?
+                pendingCompanies.map((company, i) => {
+                    return (
+                        <FoldableCompanyCard company={company} key={i} updateCompanies={getPendingCompanies}/>
+                    )
+                })
+                :
+                "no Companies"
+            }
+            
+        </div>
+    );
+}
+
+const pendingCompaniesStyle: CSSProperties = {
+    padding: "1em",
+    display: "flex",
+    width: "100%"
+}
