0390c1e7a5b02a69c498c9782c04bb201ce18e58
Joakim <j-pf@live.se>
2022-01-18 18:15:30 +0100

    added userAuth

---
+++ client/src/components/functions/reqUserAuth.tsx
@@ -0,0 +1,36 @@
+import { getAuth } from 'firebase/auth';
+import React, { CSSProperties, useContext, useState } from 'react';
+import { Navigate, useLocation, useMatch } from 'react-router-dom';
+
+type Props = {
+    children: JSX.Element
+	isLoggedIn: boolean | undefined
+
+}
+
+export default function ReqUserAuth(props: Props) {
+  
+    // const navigate = useNavigate();
+    //let location = useLocation();
+    
+    const match = useMatch("myPages/:userId/*");
+    const auth = getAuth();
+
+    if (props.isLoggedIn === undefined) {
+    
+        return <p>Spinner</p>        
+    }
+    else if(props.isLoggedIn === true && auth.currentUser?.uid === match?.params.userId) {
+        console.log("User authenticated")
+        return props.children
+    
+    }
+    else {
+    
+        return <Navigate to="/userNotFound" /* state={{ from: location }} */ replace />;
+    
+    }
+
+    
+}
+
