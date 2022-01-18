ca05f1a52c5f4764e39bd583efaa01d95872534f
Joakim <j-pf@live.se>
2022-01-18 18:13:45 +0100

    work in progress

--- client/src/components/dashboard/dashUserInfo.tsx
+++ client/src/components/dashboard/dashUserInfo.tsx
@@ -1,18 +1,29 @@
 import React, { CSSProperties, useContext, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Button from '../UI/button';
+
+type CurrentCompany = {
+    name: string
+    id: string
+}
+type Props = {
+    currentCompany: CurrentCompany | undefined
+}
 
 
-
-
-export default function DashUserInfo() {
+export default function DashUserInfo(props: Props) {
 
    
-
+    
 
 
     return (
         <div>
-            <p>UserInfo hej hej</p>
+            {props.currentCompany === undefined?
+                <Button buttonText="Ansök om att registrera ditt företag" linkTo={"registerCompany"} />
+                : 
+                null
+            }
         </div>
     );
 }
