681814eed4dfaf066aa93a764c3d7f41e7a05ec0
Sebastian Borg <Sebastian.borg99@hotmail.se>
2022-01-19 13:58:58 +0100

    backend funcs

--- server/controllers/stripeController.ts
+++ server/controllers/stripeController.ts
@@ -16,7 +16,7 @@ export const getAccount = async (req: any, res: any, next: any) => {
       } else {
             res.status(200).send({status: 200, message: "Ditt konto har verifierats, nu kan du ta betalt med stripe" })
       }
-
+      console.log(account)
     return account
 }
 
@@ -45,7 +45,13 @@ export const createStripeLink = async (req: any, res: any, next: any) => {
 
 export const checkOut = async (req: any, res: any, next: any) => {
 
+    const stripeId = req.body.stripeId
+    console.log(req.body)
+
     const cartItems = req.body.products
+    const companyId = req.body.companyId
+    const userId = req.body.userId
+ 
 
     const lineItems = await cartItems.map(product => {
         const lineItem = {
@@ -64,7 +70,7 @@ export const checkOut = async (req: any, res: any, next: any) => {
     console.log(lineItems)
 
     const session = await stripe.checkout.sessions.create({
-        success_url: 'http://localhost:3000',
+        success_url: `http://localhost:3000/success/${stripeId}/{CHECKOUT_SESSION_ID}`,
         cancel_url: 'http://localhost:3000',
         line_items: lineItems,
         mode: 'payment',
@@ -72,11 +78,65 @@ export const checkOut = async (req: any, res: any, next: any) => {
         payment_intent_data: {
             application_fee_amount: 10000, //NOTE: avgift vi tar per betalning, sätt den procentuell
         },
+        metadata: {'company_id': companyId, 'user_id': userId}
         }, 
         {
-            stripeAccount: 'acct_1KIvKL2ZjQKtl2fI', //NOTE: Skall skickas upp i body
+            stripeAccount: stripeId, //NOTE: Skall skickas upp i body
     });
 
     res.status(200).json({ id: session.id })
     return session
 }
+
+export const verifySession = async (req: any, res: any, next: any) => {
+    const allOrders = req.body.getOrders
+
+    const stripeAccountId = req.body.stripeId 
+
+    const sessionId = await req.body.sessionId
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        stripeAccount: stripeAccountId 
+    });
+
+    if(session.payment_status === "paid") {
+        const foundSessionId = allOrders.find(order => order.id === sessionId)
+
+        if(!foundSessionId) {
+
+            const lineItems = await stripe.checkout.sessions.listLineItems(sessionId, {
+                stripeAccount: stripeAccountId 
+            });
+            const orderProducts = await Promise.all(lineItems.data.map( async (item: any) => {
+                const product = await stripe.products.retrieve(item.price.product, {
+                    stripeAccount: stripeAccountId 
+                });
+                const orderProduct = {  
+                      name: product.name,
+                      quantity: item.quantity,
+                      description: item.description,
+                      unitPrice: item.price.unit_amount / 100
+                  }
+                  return orderProduct  
+                }))
+
+                let d = new Date(); 
+                let NoTimeDate = d.getFullYear()+'/'+(d.getMonth()+1)+'/'+d.getDate(); 
+                const newOrder = {
+                    companyId: session.metadata.company_id,
+                    customerId: session.metadata.user_id,
+                    stripeCustomerId:  session.customer, 
+                    products: orderProducts,
+                    totalPrice: session.amount_total / 100,
+                    orderDate: NoTimeDate,
+                    currency: session.currency
+                }
+                console.log("Order added", newOrder)
+                res.status(200).json({status: 200, order: newOrder, message: "Tack för din order." })
+        }
+          else {
+            console.log("Finns redan")
+            res.status(404).json("Ordern finns redan registrerad")
+        }
+    }
+}
+
--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,11 @@
 import express from "express"
 const routes = express.Router();
-import { checkOut, createStripeAcc, createStripeLink, getAccount } from './controllers/stripeController'
+import { checkOut, createStripeAcc, createStripeLink, getAccount, verifySession } from './controllers/stripeController'
 
 routes.post('/checkout', checkOut)
 routes.post('/createStripe', createStripeAcc)
 routes.post('/createStripeLink', createStripeLink)
 routes.post('/getStripeAcc', getAccount)
+routes.post('/verifySession', verifySession)
 
 export default routes
