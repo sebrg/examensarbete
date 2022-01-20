90a55441410eb0c2d07dc9e3a7fc552a3a4c893c
Joakim <j-pf@live.se>
2022-01-20 13:14:09 +0100

    added function for checking if payment enabled

--- client/src/components/dashboard/companyDash/dashForCompanyProducts.tsx
+++ client/src/components/dashboard/companyDash/dashForCompanyProducts.tsx
@@ -2,19 +2,23 @@ import React, { CSSProperties, useContext, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { FirebaseContext, FirebaseOptions } from '../../../context/firebaseContext';
 import Button from '../../UI/button';
-import { Product } from "../../../models"
-import { DocumentData } from 'firebase/firestore';
+import { Company, Product } from "../../../models"
+import { DocumentData, documentId, FieldPath, WhereFilterOp } from 'firebase/firestore';
 import DashForCompanyAddProducts from './dashForCompanyAddProduct';
 import { ProductContext, ProductOptions } from '../../../context/products/productContext';
 import { CompanyContext, CompanyOptions } from '../../../context/companies/companyContext';
 import ProductCard from '../../UI/productCard';
 import EditPopup from './editPopup';
+import { FbQuery } from '../../../types';
 
 type Alternatives = "show" | "add"
 
-export default function DashForCompanyProducts() {
+type Props = {
+    currentCompany: Pick<Company, "name" | "id"> | undefined
+}
 
-    //const fbFuncs: FirebaseOptions = useContext(FirebaseContext)
+export default function DashForCompanyProducts(props: Props) {
+
     const productContext: ProductOptions = useContext(ProductContext)
     const companyContext: CompanyOptions = useContext(CompanyContext)
 
@@ -22,13 +26,35 @@ export default function DashForCompanyProducts() {
     const [productAlternativ, setProductAlternativ] = useState<Alternatives>("show")
     const [products, setProducts] = useState<Product[]>()
     const [editProductTarget, setEditProductTarget] = useState<Product>()
+    const [currentCompanyEnabled, setCurrentCompanyEnabled] = useState<Boolean>()
 
-    const getProducts = async () => {
-        const currentUserCompany = await companyContext.getCurrentUserCompany()
-        const products = await productContext.functions.getProductsFromCompany(currentUserCompany[0].id as string)
-        setProducts(products)
+
+    const checkCompany = async () => {
+        const queryOne: FbQuery = {
+            fieldPath: documentId(),
+            opStr: "==",
+            value: props.currentCompany?.id as string
+        }
+        const queryTwo: FbQuery = {
+            fieldPath: "payments.enabled",
+            opStr: "==",
+            value: true
+        }
+        const getEnabledCompany = await companyContext.getCompany("companies", {...queryOne}, {...queryTwo})
+        if (getEnabledCompany.length > 0) {
+            setCurrentCompanyEnabled(true)
+        }
+        else {
+            setCurrentCompanyEnabled(false)
+        }
     }
 
+
+    const getProducts = async () => {
+        //const currentUserCompany = await companyContext.getCurrentUserCompany()
+        const products = await productContext.functions.getProductsFromCompany(props.currentCompany?.id as string)
+        setProducts(products)
+    }
 
     const renderProducts = () => { //FIXME: make this div returned to a component
         const findId = (product: Product) => { return products?.find((productInArray) => productInArray === product as Product) }
@@ -45,63 +71,62 @@ export default function DashForCompanyProducts() {
     }
 
     useEffect(() => {
-        getProducts()
+        checkCompany()
     }, [])
 
     useEffect(() => {
-        console.log("useEffect in dashforcompany: ", editProductTarget)
-        
-    }, [editProductTarget])
-
-    useEffect(() => {
-        console.log(products)
-    }, [products])
+        if(currentCompanyEnabled) {
+            getProducts()
+        }
+    }, [currentCompanyEnabled])
 
 
     return (
-        <div id="dashProducts" style={dashProductsStyle}>
-            <div id="dashProductsBtnDiv" style={{width: "30%"}}>
-                <Button 
-                    margin='0 0 0.5em 0' 
-                    /* width="30%" */ 
-                    buttonText='See all products' 
-                    bgColor={productAlternativ === "show"? "rgb(49, 52, 68)" : ""}
-                    onClick={() => setProductAlternativ('show')} 
-                    
-                />
+        currentCompanyEnabled === undefined?
+            <p>....Spinner....</p>
+            : currentCompanyEnabled?
+                <div id="dashProducts" style={dashProductsStyle}>
+                    <div id="dashProductsBtnDiv" style={{width: "30%"}}>
+                        <Button 
+                            margin='0 0 0.5em 0' 
+                            /* width="30%" */ 
+                            buttonText='Se alla produkter' 
+                            bgColor={productAlternativ === "show"? "rgb(49, 52, 68)" : ""}
+                            onClick={() => setProductAlternativ('show')} 
+                            
+                        />
 
-                <Button 
-                    onClick={() => setProductAlternativ('add')} 
-                    margin='0 0 0.5em 0' 
-                    /* width="30%"  */
-                    buttonText='Add product'
-                    bgColor={productAlternativ === "add"? "rgb(49, 52, 68)" : ""}
+                        <Button 
+                            onClick={() => setProductAlternativ('add')} 
+                            margin='0 0 0.5em 0' 
+                            /* width="30%"  */
+                            buttonText='Lägg till produkt'
+                            bgColor={productAlternativ === "add"? "rgb(49, 52, 68)" : ""}
 
-                />
-            </div>
-
-            {
-                productAlternativ === "show"? 
-                   
-                    <div id="dashShowProducts" className='noScrollBar' style={dashShowProductsStyle}>
-                        { renderProducts() }
-                        { editPopupOpen?
-                            <EditPopup product={editProductTarget as Product} setEditPopupOpen={(bool: boolean) => setEditPopupOpen(bool)} />
-                            : null
-                        }
+                        />
                     </div>
 
-                    :
+                    {productAlternativ === "show"? 
+                        
+                        <div id="dashShowProducts" className='noScrollBar' style={dashShowProductsStyle}>
+                            { renderProducts() }
+                            { editPopupOpen?
+                                <EditPopup product={editProductTarget as Product} setEditPopupOpen={(bool: boolean) => setEditPopupOpen(bool)} />
+                                : null
+                            }
+                        </div>
 
-                productAlternativ === "add"?
-                    
-                    <DashForCompanyAddProducts />
-                
-                    :
-                
-                    null
-            }
-        </div>
+                        : productAlternativ === "add"?
+                            
+                            <DashForCompanyAddProducts />
+                            : null
+                    }
+                </div>
+                : 
+                <div id="dashProducts" style={dashProductsStyle}>
+                    Du måste aktivera en betalningsmetod för att kunna lägga till produkter till ditt företag.
+                    Om du redan har aktiverat en betalningsmetod behöver den bli godkänd innan du får tillgång.
+                </div>
     );  
 }
 
