74995307c37de88ca00cc0db93519c0570bd01de
Joakim <j-pf@live.se>
2022-01-20 13:14:39 +0100

    added typings folder structure

---
+++ client/src/types/fbQuery.tsx
@@ -0,0 +1,7 @@
+import { FieldPath, WhereFilterOp } from "firebase/firestore";
+
+export type FbQuery = {
+    fieldPath: string | FieldPath
+    opStr: WhereFilterOp, 
+    value: string | string[] | boolean,
+}
