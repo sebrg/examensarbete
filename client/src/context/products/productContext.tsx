518b675d23a0a74fa68387798a8a6ed0f7b3f96b
Joakim <j-pf@live.se>
2022-01-21 17:25:52 +0100

    added type

--- client/src/context/products/productContext.tsx
+++ client/src/context/products/productContext.tsx
@@ -1,6 +1,7 @@
 import { DocumentData, FieldPath, WhereFilterOp } from 'firebase/firestore'
 import { createContext } from 'react'
 import { Company, Product } from "../../models"
+import { StatusObject } from '../../types'
 
 
 export interface ProductOptions {
@@ -9,7 +10,7 @@ export interface ProductOptions {
 }
 
 export interface ProductFunctions {
-    addProduct: (product: Product) => void
+    addProduct: (product: Product) => Promise<StatusObject>
     getProductsFromCompany: (companyId: string) => Promise<Product[]>
     upLoadImg: (file: any) => void 
     //getSingleProduct: (docId: string) => Promise<Product | undefined> //FIXME: should not be allowed to be undefined
